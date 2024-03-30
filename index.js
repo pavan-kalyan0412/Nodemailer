4bab4cc63aa8c3d7609e4264d8cf6072621b1bb9
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-03-30 14:12:03 +0530

    add message route

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const emailRoutes = require('./routes/emailRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost:27017/emailDB');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+async function connectToDatabase() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/nodemail');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
+
+connectToDatabase();
 
 app.use('/email', emailRoutes);
+app.use('/messages', messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
