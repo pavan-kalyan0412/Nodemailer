89252d854137c0f0b398d662fe0f151ecc9bcd4a
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-02 21:51:33 +0530

    integrated database with server

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -2,43 +2,57 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
+const Email = require('../models/Email.js')
 
 dotenv.config();
 
-router.post('/send', (req, res) => {
-  const { name, email } = req.body;
-  
+router.post('/send', async (req, res) => {
+  const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.User,
-      pass: process.env.Pass,
-    }
+  // Create a new Email document
+  const newEmail = new Email({
+    name,
+    email,
+    message,
   });
 
-  const mailOptions = {
-    from: process.env.User,
-    to: email,
-    subject: 'Thank you for contacting us!',
-    html: `
-      <p style="font-size: 16px; color: #333; font-weight: bold;">Dear ${name},</p>
-      <p style="font-size: 14px; color: #666;">Thank you for reaching out to us. We have received your message and will get back to you as soon as possible.</p>
-      <p style="font-size: 14px; color: #666;">Best regards,</p>
-    `
-  };
-  
-  
+  try {
+    // Save the email data to the database
+    await newEmail.save();
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send({ message: 'Error sending email' });
-    } else {
-      console.log('Email sent to user:', info.response);
-      res.status(200).send({ message: 'Email sent successfully' });
-    }
-  });
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.User,
+        pass: process.env.Pass,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.User,
+      to: email,
+      subject: 'Thank you for contacting us!',
+      html: `
+        <p style="font-size: 16px; color: #333; font-weight: bold;">Dear ${name},</p>
+        <p style="font-size: 14px; color: #666;">Thank you for reaching out to us. We have received your message and will get back to you as soon as possible.</p>
+        <p style="font-size: 14px; color: #666;">Best regards,</p>
+      `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send({ message: 'Error sending email' });
+      } else {
+        console.log('Email sent to user:', info.response);
+        res.status(200).send({ message: 'Email sent successfully' });
+      }
+    });
+  } catch (error) {
+    console.error('Error saving email to database:', error);
+    res.status(500).send({ message: 'Error saving email to database' });
+  }
 });
+
 
 module.exports = router;
